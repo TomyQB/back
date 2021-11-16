be70005d549f6a3ef6bcb901483386835b152e69
TomyQB <montialvo@gmail.com>
2021-11-16 16:09:52 +0100

    converter saveCourt

--- src/main/java/com/appuntate/back/service/TimeIntervalService.java
+++ src/main/java/com/appuntate/back/service/TimeIntervalService.java
@@ -34,9 +34,9 @@ public class TimeIntervalService {
     public List<TimeInterval> createTimeIntervalByHours(CourtDTO dto, Court court) {
         List<TimeInterval> timeIntervals = new ArrayList<>();
 
-        int startHourInt = HourConverter.stringToHour(dto.getStartHour());
-        int endHourInt = HourConverter.stringToHour(dto.getEndHour());
-        int intervalInt = HourConverter.stringToHour(dto.getInterval());
+        int startHourInt = HourConverter.stringToHour(HourConverter.dateToHours(dto.getStartHour()));
+        int endHourInt = HourConverter.stringToHour(HourConverter.dateToHours(dto.getEndHour()));
+        int intervalInt = HourConverter.stringToHour(HourConverter.dateToHours(dto.getEndHour()));
 
         while(startHourInt < endHourInt) {
             int auxEnHour = calculateEndHour(startHourInt, intervalInt);
